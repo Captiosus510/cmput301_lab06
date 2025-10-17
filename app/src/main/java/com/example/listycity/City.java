f3cebd26c563b9c18596df64b829569b84b0f5b7
MahdLinux <afzalmahd@gmail.com>
2025-10-17 02:16:23 +0200

    commented

--- app/src/main/java/com/example/listycity/City.java
+++ app/src/main/java/com/example/listycity/City.java
@@ -10,17 +10,38 @@ public class City implements Comparable<City>{
         this.city = city;
         this.province = province;
     }
+    /**
+     * This returns the name of the city.
+     * @return
+     * Return the name of the city.
+     */
     public String getCityName(){
         return this.city;
     }
+    /**
+     * This returns the name of the province.
+     * @return
+     * Return the name of the province.
+     */
     public String getProvinceName(){
         return this.province;
     }
 
+    /**
+     * This compares two cities.
+     * @param o the object to be compared.
+     * @return
+     */
     @Override
     public int compareTo(City o) {
         return this.city.compareTo(o.getCityName()); // this.city refers to the city name
     }
+
+    /**
+     * This returns a string representation of the city.
+     * @param o
+     * @return
+     */
     @Override
     public boolean equals(Object o) {
         if (o == this) {
